f73b3fe6c297020d2fcef06cd97cf088f4a7b254
Shabir Mean <shabir_tck09@hotmail.com>
2017-11-05 22:46:19 -0500

    Moved publisher to a separate inner class

--- src/main/java/net/floodlightcontroller/mactracker/FlowModifier.java
+++ src/main/java/net/floodlightcontroller/mactracker/FlowModifier.java
@@ -33,17 +33,16 @@ public class FlowModifier implements MqttCallback {
 
     void init() {
         logger = LoggerFactory.getLogger(FlowModifier.class);
-        try {
-            mqttClient = new MqttClient(BROKER_URI, MqttClient.generateClientId(), new MemoryPersistence());
-            mqttClient.setCallback(this);
-            this.subscribe();
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-    }
 
-    private void subscribe() {
         Runnable subscriber = () -> {
+            try {
+                mqttClient = new MqttClient(BROKER_URI, MqttClient.generateClientId(), new MemoryPersistence());
+                mqttClient.setCallback(this);
+//                this.subscribe();
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+
             while(!mqttClient.isConnected()) {
                 try {
                     MqttConnectOptions options = new MqttConnectOptions();
@@ -71,92 +70,34 @@ public class FlowModifier implements MqttCallback {
         subscriberThread.start();
     }
 
-    private void handleEventRequest(String customer, String eventIdentifier) {
-        switch (customer.toUpperCase()) {
-            case BELL_FLOW:
-                //TODO:: Setup stuff
-                String flowEntry1 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-1-2-1\"," +
-                        "\"priority\":\"32768\",\"in_port\":\"1\",\"active\":\"true\", \"eth_type\":\"0x0800\"," +
-                        "\"eth_src\":\"1e:c7:d9:4c:cb:7d\", \"eth_dst\":\"62:a4:25:4c:7b:a0\", " +
-                        "\"ipv4_src\":\"192.168.1.1\",\"ipv4_dst\":\"192.168.1.2\", \"actions\":\"output=normal\"}";
-
-                String flowEntry2 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-1-other\"," +
-                        "\"priority\":\"32767\",\"in_port\":\"1\",\"active\":\"true\", \"eth_type\":\"0x0800\"," +
-                        "\"eth_src\":\"1e:c7:d9:4c:cb:7d\", \"ipv4_src\":\"192.168.1.1\", " +
-                        "\"instruction_goto_table\":\"1\"}";
-
-                String flowEntry3 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-1=tab-1\"," +
-                        "\"table\":\"1\"," + "\"priority\":\"32768\",\"in_port\":\"1\",\"active\":\"true\", " +
-                        "\"eth_type\":\"0x0800\"," + "\"eth_src\":\"1e:c7:d9:4c:cb:7d\", " +
-                        "\"ipv4_src\":\"192.168.1.1\", \"actions\":\"\"}";
-
-                String flowEntry4 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-1-2-3\"," +
-                        "\"priority\":\"32768\",\"in_port\":\"2\",\"active\":\"true\", \"eth_type\":\"0x0800\"," +
-                        "\"eth_src\":\"62:a4:25:4c:7b:a0\", \"eth_dst\":\"2a:74:6c:22:0f:96\", " +
-                        "\"ipv4_src\":\"192.168.1.2\",\"ipv4_dst\":\"192.168.1.3\", \"actions\":\"output=normal\"}";
-
-                String flowEntry5 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-1-2-2\"," +
-                        "\"priority\":\"32768\",\"in_port\":\"2\",\"active\":\"true\", \"eth_type\":\"0x0800\"," +
-                        "\"eth_src\":\"62:a4:25:4c:7b:a0\", \"eth_dst\":\"1e:c7:d9:4c:cb:7d\", " +
-                        "\"ipv4_src\":\"192.168.1.2\",\"ipv4_dst\":\"192.168.1.1\", \"actions\":\"output=normal\"}";
-
-                String flowEntry6 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-2-other\"," +
-                        "\"priority\":\"32767\",\"in_port\":\"2\",\"active\":\"true\", \"eth_type\":\"0x0800\"," +
-                        "\"eth_src\":\"62:a4:25:4c:7b:a0\", \"ipv4_src\":\"192.168.1.2\", " +
-                        "\"instruction_goto_table\":\"1\"}";
-
-                String flowEntry7 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-2=tab-1\"," +
-                        "\"table\":\"1\",\"priority\":\"32768\",\"in_port\":\"2\",\"active\":\"true\", " +
-                        "\"eth_type\":\"0x0800\",\"eth_src\":\"62:a4:25:4c:7b:a0\", \"ipv4_src\":\"192.168.1.2\"," +
-                        " \"actions\":\"\"}";
-
-                doPost(flowEntry1);
-                doPost(flowEntry2);
-                doPost(flowEntry3);
-                doPost(flowEntry4);
-                doPost(flowEntry5);
-                doPost(flowEntry6);
-                doPost(flowEntry7);
-                break;
-
-            case FIDO_FLOW:
-                //TODO:: Setup stuff
-                break;
-        }
-        publishOK(eventIdentifier, "192.168.1.1");
-    }
-
-    private void publishOK(String eventIdentifier, String ingressIP) {
-        try {
-            mqttClient.publish(PUBLISH_TOPIC, (eventIdentifier + ":" + ingressIP).getBytes(UTF_8), 2, false);
-        } catch (MqttException | UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private boolean doPost(String flowEntry) {
-        try {
-            URL obj = new URL(OVS_ENDPOINT);
-            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-            connection.setRequestMethod("POST");
-            // Send post request
-            connection.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-            wr.writeBytes(flowEntry);
-            wr.flush();
-            wr.close();
-
-            int responseCode = connection.getResponseCode();
-            System.out.println("\nSending 'POST' request to URL : " + OVS_ENDPOINT);
-            System.out.println("Post parameters : \n" + flowEntry);
-//            System.out.println("Response Code : " + responseCode);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-
-    }
+//    private void subscribe() {
+//        Runnable subscriber = () -> {
+//            while(!mqttClient.isConnected()) {
+//                try {
+//                    MqttConnectOptions options = new MqttConnectOptions();
+//                    options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+//                    mqttClient.connect(options);
+//
+//                    if (mqttClient.isConnected()) {
+//                        logger.info("############# STARTED MQTT Listener...");
+//                        mqttClient.subscribe(SUBSCRIBE_TOPIC, 1);
+//                    }
+//                } catch (MqttException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                try {
+//                    Thread.sleep(3000);
+//                } catch (InterruptedException ex) {
+//                    logger.error("MQTT Connect-Thread Sleep Interrupt Exception.");
+//                }
+//            }
+//        };
+//
+//        Thread subscriberThread = new Thread(subscriber);
+//        subscriberThread.setDaemon(true);
+//        subscriberThread.start();
+//    }
 
     @Override
     public void connectionLost(Throwable throwable) {
@@ -169,11 +110,109 @@ public class FlowModifier implements MqttCallback {
         String messageIncoming = mqttMessage.toString();
         String eventIdentifier = topic.substring(topic.lastIndexOf(File.separator) + 1);
         System.out.println(topic + " : " + messageIncoming);
-        handleEventRequest(messageIncoming, eventIdentifier);
+        MqttClient tempClient = new MqttClient(BROKER_URI, MqttClient.generateClientId(), new MemoryPersistence());
+        new MqttPublisher(tempClient).handleEventRequest(messageIncoming, eventIdentifier);
     }
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+    }
+
+    private class MqttPublisher{
+        MqttClient mqttPublisherClient;
+
+        MqttPublisher(MqttClient mqttPublisherClient){
+            this.mqttPublisherClient = mqttPublisherClient;
+        }
+
+        private void handleEventRequest(String customer, String eventIdentifier) {
+            switch (customer.toUpperCase()) {
+                case BELL_FLOW:
+                    //TODO:: Setup stuff
+                    String flowEntry1 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-1-2-1\"," +
+                            "\"priority\":\"32768\",\"in_port\":\"1\",\"active\":\"true\", \"eth_type\":\"0x0800\"," +
+                            "\"eth_src\":\"1e:c7:d9:4c:cb:7d\", \"eth_dst\":\"62:a4:25:4c:7b:a0\", " +
+                            "\"ipv4_src\":\"192.168.1.1\",\"ipv4_dst\":\"192.168.1.2\", \"actions\":\"output=normal\"}";
+
+                    String flowEntry2 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-1-other\"," +
+                            "\"priority\":\"32767\",\"in_port\":\"1\",\"active\":\"true\", \"eth_type\":\"0x0800\"," +
+                            "\"eth_src\":\"1e:c7:d9:4c:cb:7d\", \"ipv4_src\":\"192.168.1.1\", " +
+                            "\"instruction_goto_table\":\"1\"}";
+
+                    String flowEntry3 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-1=tab-1\"," +
+                            "\"table\":\"1\"," + "\"priority\":\"32768\",\"in_port\":\"1\",\"active\":\"true\", " +
+                            "\"eth_type\":\"0x0800\"," + "\"eth_src\":\"1e:c7:d9:4c:cb:7d\", " +
+                            "\"ipv4_src\":\"192.168.1.1\", \"actions\":\"\"}";
+
+                    String flowEntry4 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-1-2-3\"," +
+                            "\"priority\":\"32768\",\"in_port\":\"2\",\"active\":\"true\", \"eth_type\":\"0x0800\"," +
+                            "\"eth_src\":\"62:a4:25:4c:7b:a0\", \"eth_dst\":\"2a:74:6c:22:0f:96\", " +
+                            "\"ipv4_src\":\"192.168.1.2\",\"ipv4_dst\":\"192.168.1.3\", \"actions\":\"output=normal\"}";
+
+                    String flowEntry5 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-1-2-2\"," +
+                            "\"priority\":\"32768\",\"in_port\":\"2\",\"active\":\"true\", \"eth_type\":\"0x0800\"," +
+                            "\"eth_src\":\"62:a4:25:4c:7b:a0\", \"eth_dst\":\"1e:c7:d9:4c:cb:7d\", " +
+                            "\"ipv4_src\":\"192.168.1.2\",\"ipv4_dst\":\"192.168.1.1\", \"actions\":\"output=normal\"}";
+
+                    String flowEntry6 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-2-other\"," +
+                            "\"priority\":\"32767\",\"in_port\":\"2\",\"active\":\"true\", \"eth_type\":\"0x0800\"," +
+                            "\"eth_src\":\"62:a4:25:4c:7b:a0\", \"ipv4_src\":\"192.168.1.2\", " +
+                            "\"instruction_goto_table\":\"1\"}";
+
+                    String flowEntry7 = "{\"switch\":\"00:00:d6:ed:a6:a2:0c:44\",\"name\":\"flow-2=tab-1\"," +
+                            "\"table\":\"1\",\"priority\":\"32768\",\"in_port\":\"2\",\"active\":\"true\", " +
+                            "\"eth_type\":\"0x0800\",\"eth_src\":\"62:a4:25:4c:7b:a0\", \"ipv4_src\":\"192.168.1.2\"," +
+                            " \"actions\":\"\"}";
+
+                    doPost(flowEntry1);
+                    doPost(flowEntry2);
+                    doPost(flowEntry3);
+                    doPost(flowEntry4);
+                    doPost(flowEntry5);
+                    doPost(flowEntry6);
+                    doPost(flowEntry7);
+                    break;
+
+                case FIDO_FLOW:
+                    //TODO:: Setup stuff
+                    break;
+            }
+            publishOK(eventIdentifier, "192.168.1.1");
+        }
+
+
+        private boolean doPost(String flowEntry) {
+            try {
+                URL obj = new URL(OVS_ENDPOINT);
+                HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+                connection.setRequestMethod("POST");
+                // Send post request
+                connection.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+                wr.writeBytes(flowEntry);
+                wr.flush();
+                wr.close();
+
+                int responseCode = connection.getResponseCode();
+                System.out.println("\nSending 'POST' request to URL : " + OVS_ENDPOINT);
+                System.out.println("Post parameters : \n" + flowEntry);
+//            System.out.println("Response Code : " + responseCode);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+            return true;
+
+        }
+
+        private void publishOK(String eventIdentifier, String ingressIP) {
+            try {
+                mqttPublisherClient.publish(PUBLISH_TOPIC, (eventIdentifier + ":" + ingressIP).getBytes(UTF_8), 2, false);
+            } catch (MqttException | UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
