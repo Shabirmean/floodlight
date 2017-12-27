be4b37d20669af5c14a1857b2dbc1d3b422e155c
Shabir Mean <shabir_tck09@hotmail.com>
2017-12-27 01:01:22 -0500

    Debug msges added to validate MQTT msg parsing

--- src/main/java/net/floodlightcontroller/cienaflowcontroller/FlowControllerConstants.java
+++ src/main/java/net/floodlightcontroller/cienaflowcontroller/FlowControllerConstants.java
@@ -13,12 +13,13 @@ class FlowControllerConstants {
     public static final String ZERO = "0";
     static final String CIENA = "ciena";
     static final String CMANAGER_FMANAGER = "cm_fm";
+    static final String FMANAGER_CMANAGER = "fm_cm";
     static final String REQUEST = "request";
     static final String RESPONSE = "response";
 
     static final String MQTT_BROKER_URI = "tcp://localhost:1883";
     static final String MQTT_SUBSCRIBE_TOPIC = CIENA + SEP + CMANAGER_FMANAGER + SEP + REQUEST + SEP + "*";
-    static final String MQTT_PUBLISH_TOPIC = CIENA + SEP + CMANAGER_FMANAGER + SEP + RESPONSE;
+    static final String MQTT_PUBLISH_TOPIC = CIENA + SEP + FMANAGER_CMANAGER + SEP + RESPONSE;
     static final String RESPONSE_MSG_FORMAT = "{\"eventId\":\"%s\",\"status\":\"%s\"}";
 
     static final String JSON_ATTRIB_CUSTOMER = "customer";
